e18a92faa3c2aecad0b230adc4c396e8c6a2067b
David17tgk <davidzimnitckic@gmail.com>
2025-02-24 22:47:43 +0300

    grge

---
+++ js/index5.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector('.header__icon')
+let nav = document.querySelector('.header__nav')
+let header = document.querySelector('.header') 
+let icon = document.querySelector('.header__icon > .fa-solid')
+let body = document.querySelector('body')
+
+
+menu.addEventListener('click', function() {
+    nav.classList.toggle('nav__active')
+    header.classList.toggle('header__active')
+    icon.classList.toggle('fa-xmark')
+    body.classList.toggle('no-scroll')
+})
+
